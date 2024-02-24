751c21ff1bdc3855af79d086d2ab0e1e0be10e97
Po-Lin Chiang <polin.chiang@vysioneer.com>
2024-02-25 00:25:55 +0800

    bugfix: rename class as className

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -14,7 +14,7 @@ const Icon = () => {
       fill='none'
       stroke='currentColor'
       strokeWidth='2'
-      class='absolute left-1 top-1 h-5 w-5 text-indigo-600'
+      className='absolute left-1 top-1 h-5 w-5 text-indigo-600'
     >
       <path d='M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z'></path>
       <path d='M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z'></path>
@@ -31,50 +31,50 @@ const Model = (props: { path: string }) => {
 export default function About() {
   return (
     <Layout>
-      <div class='overflow-hidden bg-white py-24 sm:py-32'>
-        <div class='mx-auto max-w-7xl px-6 lg:px-8'>
-          <div class='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
-            <div class='lg:pr-8 lg:pt-4'>
-              <div class='lg:max-w-lg'>
-                <h2 class='text-base font-semibold leading-7 text-indigo-600'>
+      <div className='overflow-hidden bg-white py-24 sm:py-32'>
+        <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+          <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
+            <div className='lg:pr-8 lg:pt-4'>
+              <div className='lg:max-w-lg'>
+                <h2 className='text-base font-semibold leading-7 text-indigo-600'>
                   Dutch Formosa (1624 - 1668)
                 </h2>
-                <p class='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
                   Fort Provintia
                 </p>
-                <p class='mt-6 text-lg leading-8 text-gray-600'>
+                <p className='mt-6 text-lg leading-8 text-gray-600'>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Maiores impedit perferendis suscipit eaque, iste dolor
                   cupiditate blanditiis ratione.
                 </p>
-                <dl class='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
-                  <div class='relative pl-9'>
-                    <dt class='inline font-semibold text-gray-900'>
+                <dl className='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
+                  <div className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
                       <Icon />
                       Status.
                     </dt>
-                    <dd class='inline'>
+                    <dd className='inline'>
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                       Maiores impedit perferendis suscipit eaque, iste dolor
                       cupiditate blanditiis ratione.
                     </dd>
                   </div>
-                  <div class='relative pl-9'>
-                    <dt class='inline font-semibold text-gray-900'>
+                  <div className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
                       <Icon />
                       Government.
                     </dt>
-                    <dd class='inline'>
+                    <dd className='inline'>
                       Anim aute id magna aliqua ad ad non deserunt sunt. Qui
                       irure qui lorem cupidatat commodo.
                     </dd>
                   </div>
-                  <div class='relative pl-9'>
-                    <dt class='inline font-semibold text-gray-900'>
+                  <div className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
                       <Icon />
                       Historical era.
                     </dt>
-                    <dd class='inline'>
+                    <dd className='inline'>
                       Ac tincidunt sapien vehicula erat auctor pellentesque
                       rhoncus. Et magna sit morbi lobortis.
                     </dd>
@@ -96,47 +96,47 @@ export default function About() {
                 <Environment preset='sunset' />
               </Suspense>
             </Canvas>
-            <div class='lg:pr-8 lg:pt-4'>
-              <div class='lg:max-w-lg'>
-                <h2 class='text-base font-semibold leading-7 text-indigo-600'>
+            <div className='lg:pr-8 lg:pt-4'>
+              <div className='lg:max-w-lg'>
+                <h2 className='text-base font-semibold leading-7 text-indigo-600'>
                   Taiwan under Qing rule (1683 - 1895)
                 </h2>
-                <p class='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
                   Chhiah Khàm Lâu
                 </p>
-                <p class='mt-6 text-lg leading-8 text-gray-600'>
+                <p className='mt-6 text-lg leading-8 text-gray-600'>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Maiores impedit perferendis suscipit eaque, iste dolor
                   cupiditate blanditiis ratione.
                 </p>
-                <dl class='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
-                  <div class='relative pl-9'>
-                    <dt class='inline font-semibold text-gray-900'>
+                <dl className='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
+                  <div className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
                       <Icon />
                       Status.
                     </dt>
-                    <dd class='inline'>
+                    <dd className='inline'>
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                       Maiores impedit perferendis suscipit eaque, iste dolor
                       cupiditate blanditiis ratione.
                     </dd>
                   </div>
-                  <div class='relative pl-9'>
-                    <dt class='inline font-semibold text-gray-900'>
+                  <div className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
                       <Icon />
                       Government.
                     </dt>
-                    <dd class='inline'>
+                    <dd className='inline'>
                       Anim aute id magna aliqua ad ad non deserunt sunt. Qui
                       irure qui lorem cupidatat commodo.
                     </dd>
                   </div>
-                  <div class='relative pl-9'>
-                    <dt class='inline font-semibold text-gray-900'>
+                  <div className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
                       <Icon />
                       Historical era.
                     </dt>
-                    <dd class='inline'>
+                    <dd className='inline'>
                       Ac tincidunt sapien vehicula erat auctor pellentesque
                       rhoncus. Et magna sit morbi lobortis.
                     </dd>
@@ -144,47 +144,47 @@ export default function About() {
                 </dl>
               </div>
             </div>
-            <div class='lg:pr-8 lg:pt-4'>
-              <div class='lg:max-w-lg'>
-                <h2 class='text-base font-semibold leading-7 text-indigo-600'>
+            <div className='lg:pr-8 lg:pt-4'>
+              <div className='lg:max-w-lg'>
+                <h2 className='text-base font-semibold leading-7 text-indigo-600'>
                   Taiwan R.O.C. (1945 - now)
                 </h2>
-                <p class='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
                   Chihkan Tower
                 </p>
-                <p class='mt-6 text-lg leading-8 text-gray-600'>
+                <p className='mt-6 text-lg leading-8 text-gray-600'>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Maiores impedit perferendis suscipit eaque, iste dolor
                   cupiditate blanditiis ratione.
                 </p>
-                <dl class='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
-                  <div class='relative pl-9'>
-                    <dt class='inline font-semibold text-gray-900'>
+                <dl className='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
+                  <div className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
                       <Icon />
                       Status.
                     </dt>
-                    <dd class='inline'>
+                    <dd className='inline'>
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                       Maiores impedit perferendis suscipit eaque, iste dolor
                       cupiditate blanditiis ratione.
                     </dd>
                   </div>
-                  <div class='relative pl-9'>
-                    <dt class='inline font-semibold text-gray-900'>
+                  <div className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
                       <Icon />
                       Government.
                     </dt>
-                    <dd class='inline'>
+                    <dd className='inline'>
                       Anim aute id magna aliqua ad ad non deserunt sunt. Qui
                       irure qui lorem cupidatat commodo.
                     </dd>
                   </div>
-                  <div class='relative pl-9'>
-                    <dt class='inline font-semibold text-gray-900'>
+                  <div className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
                       <Icon />
                       Historical era.
                     </dt>
-                    <dd class='inline'>
+                    <dd className='inline'>
                       Ac tincidunt sapien vehicula erat auctor pellentesque
                       rhoncus. Et magna sit morbi lobortis.
                     </dd>
