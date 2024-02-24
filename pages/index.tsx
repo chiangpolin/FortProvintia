771c192e2b53baec1f85d4e6bf1ddd3ac091e694
Po-Lin Chiang <polin.chiang@vysioneer.com>
2024-02-25 00:08:02 +0800

    init

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Layout from '@/components/layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className='relative flex items-center justify-center h-screen overflow-hidden'>
+        <Image
+          src='/static/cover_01_1280x720.jpg'
+          layout='fill'
+          objectFit='contain'
+          alt='Fort Provintia'
+        />
+      </div>
+      <div class='fixed top-1/2 right-1/2 z-20'>
+        <p class='text-white text-4xl font-mono text-center'>
+          Fort Provintia 1653
+        </p>
+      </div>
+    </Layout>
+  );
+}
