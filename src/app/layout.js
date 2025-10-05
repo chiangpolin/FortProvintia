9e6b0ddb7d40db90a3053244e22b88e8a01484f8
Po-Lin Chiang <polin.chiang1996@gmail.com>
2025-10-05 14:11:29 +0800

    add title

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <title>Fort Provintia</title>
+      </head>
       <body>{children}</body>
     </html>
   );
