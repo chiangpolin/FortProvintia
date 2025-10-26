18fc418d16311e1f1a598d76a765cd730f0aba19
Po-Lin Chiang <polin.chiang1996@gmail.com>
2025-10-27 01:32:46 +0800

    add apis

---
+++ backend/image-to-image.py
@@ -0,0 +1,49 @@
+import base64
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+engine_id = "stable-diffusion-xl-1024-v1-0"
+api_host = os.getenv("API_HOST", "https://api.stability.ai")
+api_key = os.getenv("API_KEY")
+input_path = os.getenv("INPUT_IMAGE_PATH")
+output_path = os.getenv("OUTPUT_IMAGE_PATH")
+
+if api_key is None:
+    raise Exception("Missing Stability API key.")
+
+prompt = (
+    "17th century"
+    "Dutch colonial fort"
+    "Preserve original structure and proportions"
+)
+
+response = requests.post(
+    f"{api_host}/v1/generation/{engine_id}/image-to-image",
+    headers={
+        "Accept": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    },
+    files={
+        "init_image": open(input_path, "rb")
+    },
+    data={
+        "image_strength": 0.4,
+        "init_image_mode": "IMAGE_STRENGTH",
+        "text_prompts[0][text]": prompt,
+        "cfg_scale": 8,
+        "samples": 1,
+        "steps": 30,
+    }
+)
+
+if response.status_code != 200:
+    raise Exception("Non-200 response: " + str(response.text))
+
+data = response.json()
+
+for i, image in enumerate(data["artifacts"]):
+    with open(output_path, "wb") as f:
+        f.write(base64.b64decode(image["base64"]))
---
+++ backend/stable-image.py
@@ -0,0 +1,48 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+input_path = os.getenv("INPUT_IMAGE_PATH")
+output_path = os.getenv("OUTPUT_IMAGE_PATH")
+
+if not api_key:
+    raise ValueError("API_KEY not found")
+
+prompt = (
+    "17th century"
+    "Dutch colonial fort"
+    "Warm sunlight"
+    "Coastal atmosphere"
+    "Preserve original structure and proportions"
+)
+
+response = requests.post(
+    "https://api.stability.ai/v2beta/stable-image/generate/sd3",
+    headers={
+        "authorization": f"Bearer {api_key}",
+        "accept": "image/*"
+    },
+    files={
+        "image": open(input_path, "rb"),
+    },
+    data={
+        "prompt": prompt,
+        "mode": 'image-to-image',
+        "strength": 0.55,
+        "output_format": "png",
+        "model": 'sd3.5-flash',
+        "seed": 100,
+        "style_preset": 'photographic',
+    },
+    timeout=600,
+)
+
+if response.status_code == 200:
+    with open(output_path, "wb") as f:
+        f.write(response.content)
+    print("success")
+else:
+    print("error:", response.text)
